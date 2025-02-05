ec17439bc1a48317355f9a2badfc40ad53c6f7f2
avnish <avirajinfotechplc@gmail.com>
2025-02-05 11:00:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let shvam=15;
+let a=70;
+let b=50;
+let c=20;
+let d="sagar";
+let e=true;
+let f=null;
+console.log(typeof(c));
+
+console.log(!(a<b || b<c));
+// tokens:
+// identifier 
+// operator
+// Arithmatic operator +,-,*,/,%
+// comparision ><<=>=,==,!=
+// assignment = +=,-=,*=,/=,%=
+//logical && || !
+// punctuator
+// literalsdddddddddddd
+// keywords
